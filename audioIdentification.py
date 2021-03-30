7740c9d615459bc96930f55444c380493ebf7b64
Wanjing Chen <wanjingchencwj@gmail.com>
2021-03-30 16:48:08 +0100

    FingerPrintBuilder almost complete

---
+++ audioIdentification.py
@@ -0,0 +1,86 @@
+import os
+import IPython.display as ipd
+import numpy as np
+import librosa
+import librosa.display
+from matplotlib import pyplot as plt
+from skimage.feature import peak_local_max
+
+def fingerprintBuilder(pathToDatabase, pathToFingerprints):
+    # Iterate the entire database and generate fingerprint for each of audio file
+    for entry in os.scandir(pathToDatabase):
+        # Load audio
+        print(entry.name)
+        y, sr = librosa.load(os.path.join(entry.path))
+
+        # --------- Use mel spectrogram for time-frequency representations ---------
+    
+        # Compute and plot mel spectrogram
+        S = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=1024, hop_length=512, win_length=1024, window='hann', n_mels=128)
+
+        # --------- Use STFT spectrogram for time-frequency representations ---------
+
+        # # Compute and plot STFT spectrogram
+        # S = np.abs(librosa.stft(y,n_fft=1024,window='hann',win_length=1024,hop_length=512))
+
+        # --------- Use CQT spectrogram for time-frequency representations ---------
+
+        # # Compute and plot CQT spectrogram
+        # S = np.abs(librosa.cqt(y, sr=sr))
+
+        # Plot spectrogram
+        plt.figure(figsize=(10, 5))
+        librosa.display.specshow(librosa.amplitude_to_db(S,ref=np.max),y_axis='linear', x_axis='time',cmap='gray_r',sr=sr)
+
+        # Detect peaks from the spectrogram and plot constellation map
+        coordinates = peak_local_max(np.log(S), min_distance=10,threshold_rel=0.05)
+        plt.figure(figsize=(10, 5))
+        plt.imshow(coordinates,cmap=plt.cm.gray_r,origin='lower')
+
+        # Simplify the audi file name
+        filename = "".join(entry.name[:-4].split("."))
+
+        # save the plotted spectrogram graph as a figure
+        plot_filename = os.path.join(os.getcwd(), pathToFingerprints, filename)
+        plt.savefig(plot_filename)
+        print(coordinates)
+
+    return None
+
+def audioIdentification(pathToQueryset, pathToFingerprints, pathToOutputtxt):
+    # Generate the fingerprints for all the queries
+    for entry in os.scandir(pathToQueryset):
+        # Load audio
+        y, sr = librosa.load(os.path.join(entry.name))
+
+        # --------- Use mel spectrogram for time-frequency representations ---------
+
+        # Compute and plot mel spectrogram
+        S = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=1024, hop_length=512, win_length=1024, window='hann', n_mels=128)
+
+        # --------- Use STFT spectrogram for time-frequency representations ---------
+
+        # # Compute and plot STFT spectrogram
+        # S = np.abs(librosa.stft(y,n_fft=1024,window='hann',win_length=1024,hop_length=512))
+
+        # --------- Use CQT spectrogram for time-frequency representations ---------
+
+        # # Compute and plot CQT spectrogram
+        # S = np.abs(librosa.cqt(y, sr=sr))
+
+        # Plot spectrogram
+        plt.figure(figsize=(10, 5))
+        librosa.display.specshow(librosa.amplitude_to_db(S,ref=np.max),y_axis='linear', x_axis='time',cmap='gray_r',sr=sr)
+
+        # Detect peaks from the spectrogram and plot constellation map
+        coordinates = peak_local_max(np.log(S), min_distance=10,threshold_rel=0.05,indices = False)
+        plt.figure(figsize=(10, 5))
+        plt.imshow(coordinates,cmap=plt.cm.gray_r,origin='lower')
+
+        # Compare fingerprints
+    return None
+
+if __name__ == "__main__":
+    pathToDatabase = '/Users/wanjing/Desktop/MSc_AI/semB/MI/cw2/database_recordings'
+    pathToFingerprints = '/Users/wanjing/Desktop/MSc_AI/semB/MI/cw2/databaseFingerprints'
+    fingerprintBuilder(pathToDatabase, pathToFingerprints)
